270c61d3d03b5f047cb7ac54b5cfa100140e6820
sarii4 <sarita248@outlook.es>
2024-05-29 14:43:14 +0200

    nksdns

--- main-page/app.js
+++ main-page/app.js
@@ -5,12 +5,23 @@ function closeGame(){
     let closeGame = window.close();
     let cerrar = document.getElementsByClassName('fa-solid fa-xmark')
 }
+//sigue reproduciéndose SOLO cuando interactúas con la pág
 
 window.addEventListener('load', function() {
-    var audio = new Audio('game-music-loop-3-144252.mp3');
+    var audio = new Audio('game-music-loop-3-144252.mp3'); // Un archivo de sonido más corto
+    audio.loop = true; //para que suene en bucle
     audio.play().then(function() {
         console.log("El sonido se está reproduciendo.");
     }).catch(function(error) {
         console.error("Error al reproducir el sonido:", error);
     });
+});
+
+// para parar la música (de momento no funciona)
+
+document.getElementsByClassName('stopSound').addEventListener('click', function() { 
+    var audio = document.getElementById('myAudio');
+    audio.pause(); // Pausa el sonido
+    audio.currentTime = 0; // Opcional: reinicia el sonido al principio
+    console.log("El sonido se ha detenido.");
 });
