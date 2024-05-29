57e505e35d21cbff8959e7b4331862df2312380e
sarii4 <sarita248@outlook.es>
2024-05-29 09:24:15 +0200

    he conseguido cerrar cuando le das a X. yasssssss

--- main-page/app.js
+++ main-page/app.js
@@ -1,3 +1,8 @@
 let playAudio = new Audio();
 
 playAudio.src = ('./sound.mp3');
+
+function closeGame(){
+    let closeGame = window.close();
+    let cerrar = document.getElementsByClassName('fa-solid fa-xmark')
+}
