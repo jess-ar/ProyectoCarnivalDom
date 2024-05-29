65dd8cc9479457e6e678cda2b791c3b48dce695e
sarii4 <sarita248@outlook.es>
2024-05-29 13:14:15 +0200

    sonido

--- main-page/app.js
+++ main-page/app.js
@@ -1,8 +1,17 @@
 let playAudio = new Audio();
-
 playAudio.src = ('./sound.mp3');
 
 function closeGame(){
     let closeGame = window.close();
     let cerrar = document.getElementsByClassName('fa-solid fa-xmark')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Crear un nuevo objeto Audio
+    var audio = new Audio('game-music-loop-3-144252.mp3');
+    
+    // Reproducir el sonido
+    audio.play().catch(function(error) {
+        console.error("Error al reproducir el sonido:", error);
+    });
+});
