d6f6fa2e06c6c70f955297a2fa97f3035382e50e
sarii4 <sarita248@outlook.es>
2024-05-31 12:44:11 +0200

    intento de musica

--- main-page/app.js
+++ main-page/app.js
@@ -1,39 +1,40 @@
-let playAudio = new Audio();
-playAudio.src = ('./sound.mp3');
-
 function closeGame(){
     let closeGame = window.close();
     let cerrar = document.getElementsByClassName('fa-solid fa-xmark')
 }
-// //ON
 
-window.addEventListener('load', function() {
-    var audio = new Audio('game-music-loop-3-144252.mp3'); // Un archivo de sonido más corto
-    audio.loop = true; //para que suene en bucle
-    audio.play().then(function() {
-        console.log("El sonido se está reproduciendo.");
-    }).catch(function(error) {
-        console.error("Error al reproducir el sonido:", error);
-    });
-});
+// musica
+let progress = document.getElementById("progress")
+let music = document.getElementById('music')
+let ctrlIcon = document.getElementById('ctrlIcon')
 
-function soundOn() {
-    // localStorage.setItem("favn1", "<a href='/details/title'><img src='https://images.unsplash.com/photo-1659560893497-bb094425bd21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80'></a>");
-    boton_add = document.getElementById("add");
-    boton_add.style.display = "none";
-    boton_remove = document.getElementById("remove");
-    boton_remove.style.display = "block";
+music.onloadeddata = function(){
+    progress.max = music.duration;
+    progress.value = music.currentTime;
 }
-
-function soundOff() {
-    // localStorage.removeItem("favn1");
-    boton_remove = document.getElementById("remove");
-    boton_remove.style.display = "none";   
-    boton_add = document.getElementById("add");
-    boton_add.style.display = "block";
-
-  }
-
+function playPause(){
+    if(music.paused){
+        music.play();
+        ctrlIcon.classList.remove('fa-solid fa-volume-high')
+        ctrlIcon.classList.add('fa-solid fa-volume-off')
+    }else{
+        music.pause();
+        ctrlIcon.classList.remove('fa-solid fa-volume-off')
+        ctrlIcon.classList.add('fa-solid fa-volume-high')
+    }
+}
+music.addEventListener('timeupdate', function(){
+    progress.value = music.currentTime;
+})    
+progress.oninput = function(){
+    music.currentTime = progress.value;
+    if(music.paused){
+        music.play();
+        ctrlIcon.classList.remove('fa-solid fa-volume-high')
+        ctrlIcon.classList.add('fa-solid fa-volume-off')
+    }
+}
+// fin musica
 
 function exit (){
     confirm("¿Quieres salir?")
